eb4cf02e347bd7b2263cd89e0270fe0fe57819a6
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-08-12 22:38:12 +0900

    test: 商品をテストごとに新規作成し、注文プロセスのテストが失敗しないように対応

--- e2e/tests/business_processes/order_process_test.js
+++ e2e/tests/business_processes/order_process_test.js
@@ -1,6 +1,27 @@
 SuiteOf('注文プロセスのテスト');
 
 Scenario('ログインし、お弁当を注文し、お弁当を受け取る', async ({ I, utils }) => {
+  // 商品名はタイムスタンプなどからユニークなものを設定する
+  // 例えば「牛ハラミ弁当-テスト-20230416120600」などのようにする
+  const itemName = `牛ハラミ弁当-テスト-${utils.now.format('YYYYMMDDHHmmss')}`;
+
+  session('お弁当屋さんのブラウザ', () => {
+    // お弁当屋さんのアカウントでログインする
+    I.amOnPage('/');
+    I.click('ログインする');
+    I.fillField('ユーザー名', 'admin');
+    I.fillField('パスワード', 'admin');
+    I.click('ログイン');
+
+    // 商品を追加する
+    // 商品説明と価格はそれぞれ「テスト用の商品です」「500」とする
+    I.click('商品を追加する');
+    I.fillField('商品名', itemName);
+    I.fillField('商品説明', 'テスト用の商品です');
+    I.fillField('価格', '500');
+    I.click('追加');
+  });
+
   // 一般ユーザーとしてログインする
   I.amOnPage('/');
   I.click('ログインする');
@@ -10,8 +31,15 @@ Scenario('ログインし、お弁当を注文し、お弁当を受け取る', a
   I.see('user1 さん');
 
   // カートに商品を入れる
-  I.fillField('カートに入れる数量', '1');
-  I.click('カートに入れる');
+  I.amOnPage('/items');
+  const itemContainer = locate('tr').withText(itemName);
+  I.fillField(
+    locate('input').after(
+      locate('label').withText('カートに入れる数量').inside(itemContainer)
+    ),
+    '10'
+  );
+  I.click('カートに入れる', itemContainer);
 
   // 受け取り情報を入力し、注文を確定する
   I.fillField('お名前（受取時に必要です）', 'ユーザー１');
@@ -24,13 +52,6 @@ Scenario('ログインし、お弁当を注文し、お弁当を受け取る', a
   const orderNo = await I.grabTextFrom('h3');
 
   session('お弁当屋さんのブラウザ', () => {
-    // お弁当屋さんのアカウントでログインする
-    I.amOnPage('/');
-    I.click('ログインする');
-    I.fillField('ユーザー名', 'admin');
-    I.fillField('パスワード', 'admin');
-    I.click('ログイン');
-
     // 注文管理画面から注文を引き渡す
     I.click('注文を管理する');
     const itemContainer = locate('aside').withText(orderNo);
