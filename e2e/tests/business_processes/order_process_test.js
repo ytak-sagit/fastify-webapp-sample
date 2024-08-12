b60b7ebc9d929baa09c408543230d98278e8fd48
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-08-12 21:24:26 +0900

    test: お弁当注文プロセスのテストを追加

---
+++ e2e/tests/business_processes/order_process_test.js
@@ -0,0 +1,19 @@
+SuiteOf('注文プロセスのテスト');
+
+Scenario('ログインし、お弁当を注文し、お弁当を受け取る', ({ I }) => {
+  I.amOnPage('/');
+  I.click('ログインする');
+  I.fillField('ユーザー名', 'user1');
+  I.fillField('パスワード', 'super-strong-passphrase');
+  I.click('ログイン');
+  I.see('user1 さん');
+
+  I.fillField('カートに入れる数量', '1');
+  I.click('カートに入れる');
+
+  I.fillField('お名前（受取時に必要です）', 'ユーザー１');
+  I.fillField('電話番号（連絡時に必要です）', '09000000000');
+  I.fillField('受け取り日', '2025/08/01');
+  I.fillField('受け取り目安時間', '12:00AM');
+  I.click('注文を確定する');
+});
