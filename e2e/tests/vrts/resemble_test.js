4d277641801fa8a1b1fe299dac87bc9fe3287703
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-08-15 23:02:06 +0900

    test: Resemble.jsのテストを追加

---
+++ e2e/tests/vrts/resemble_test.js
@@ -0,0 +1,9 @@
+// NOTE: このテストはサンプルアプリに対するE2Eテストではありません
+
+Feature('Resemble.jsのテスト');
+
+Scenario('ホームページのテスト', async ({ I }) => {
+  I.amOnPage('https://applitools.com/helloworld?diff2');
+  I.saveScreenshot('Homepage.png');
+  I.seeVisualDiff('Homepage.png', { tolerance: 0.1 });  // 0.1%までの誤差を許容
+});
